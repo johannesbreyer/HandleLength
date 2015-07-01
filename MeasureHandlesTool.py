173bc3d10aa6cb4c66e2acb7408818e30fc3ce66
Gustavo Ferreira <gustavo@hipertipo.com>
2015-07-01 11:46:03 -0300

    fixing bugs, updating icon

--- MeasureHandlesTool.py
+++ MeasureHandlesTool.py
@@ -21,13 +21,19 @@ BEZIER_ARC_CIRCLE = 0.5522847498
 #-----------
 
 def vector((x, y), angle, distance):
-    """Calculate a new position based on a given angle and distance."""
+    """
+    Calculate a new position based on a given angle and distance.
+
+    """
     _x = x + cos(radians(angle)) * distance
     _y = y + sin(radians(angle)) * distance
     return _x, _y
 
 def get_vector((x1, y1), (x2, y2)):
-    """Get the distance and angle between two points."""
+    """
+    Get the distance and angle between two points.
+
+    """
     a = x2 - x1
     b = y2 - y1
     distance = sqrt(a ** 2 + b ** 2)
@@ -51,13 +57,13 @@ class HandlesMeasures(object):
     draw_in = True
     draw_out = True
 
-    radius = 0.5
+    radius = 0.3
 
     font_size = 9
     font = "Lucida Grande Bold"
 
     stroke_width = 1
-    color = 1, 0.5, 0
+    color = 0, 0, 1
     stroke_alpha = 0.65
 
     def __init__(self, glyph):
@@ -78,32 +84,39 @@ class HandlesMeasures(object):
 
     def _draw_handles(self, scale):
 
-        d = 0.65
-
         save()
         fill(*self.color)
         stroke(None)
 
         for c in self.glyph:
             for bPoint in c.bPoints:
+
                 # get positions
                 x0, y0 = bPoint.anchor
                 w1, h1 = bPoint.bcpIn
                 w2, h2 = bPoint.bcpOut
                 x1, y1 = x0 + w1, y0 + h1
                 x2, y2 = x0 + w2, y0 + h2
+
                 # draw incoming bcp
                 if self.draw_in:
                     if w1 != 0 or h1 != 0:
                         d1 = sqrt(w1 ** 2 + h1 ** 2)
                         d1_caption = '%.2f' % d1
-                        text(d1_caption, (x0 + w1 * 0.5, y0 + h1 * d))
+                        d1_w, d1_h = textSize(d1_caption)
+                        d1_x = x0 + (w1 * 0.5) - (d1_w * 0.5)
+                        d1_y = y0 + (h1 * 0.5) - (d1_h * 0.4)
+                        textBox(d1_caption, (d1_x, d1_y, d1_w, d1_h), align='center')
+
                 # draw outcoming bcp
                 if self.draw_out:
                     if w2 != 0 or h2 != 0:
                         d2 = sqrt(w2 * w2 + h2 * h2)
                         d2_caption = '%.2f' % d2
-                        text(d2_caption, (x0 + w2 * 0.5, y0 + h2 * d))
+                        d2_w, d2_h = textSize(d2_caption)
+                        d2_x = x0 + (w2 * 0.5) - (d2_w * 0.5)
+                        d2_y = y0 + (h2 * 0.5) - (d2_h * 0.4)
+                        textBox(d2_caption, (d2_x, d2_y, d2_w, d2_h), align='center')
         # done
         restore()
 
@@ -123,55 +136,66 @@ class HandlesMeasures(object):
                 x1, y1 = x0 + w1, y0 + h1
                 x2, y2 = x0 + w2, y0 + h2
 
+                # draw box for incoming BCP
                 if self.draw_in and not int(w1) == 0 and not int(h1) == 0:
+
+                    # draw box
                     fill(None)
                     c = self.color + (self.stroke_alpha,)
                     stroke(*c)
                     rect(x0, y0, w1, h1)
                     line((x0, y0), (x1, y1))
 
-                    if y1 > y0:
-                        y1_ = (y1 + 4) * scale
-                    else:
-                        y1_ = (y1 - 14) * scale
-
-                    if x1 > x0:
-                        x1_ = (x1 + 4) * scale
-                    else:
-                        x1_ = (x1 - 19) * scale
-
+                    # draw captions
                     stroke(None)
                     fill(*self.color)
-                    text('%.2f' % abs(w1), (x0 + w1 * 0.5, y1_))
-                    text('%.2f' % abs(h1), (x1_, y0 + h1 * 0.5))
 
+                    # draw x caption
+                    x1_caption = '%.2f' % abs(w1)
+                    x1_w, x1_h = textSize(x1_caption)
+                    x1_x = x0 + (w1 * 0.5) - (x1_w * 0.5)
+                    x1_y = y1 - x1_h * 0.4
+                    textBox(x1_caption, (x1_x, x1_y, x1_w, x1_h), align='center')
+
+                    # draw y caption
+                    y1_caption = '%.2f' % abs(h1)
+                    y1_w, y1_h = textSize(y1_caption)
+                    y1_x = x1 - (y1_w * 0.5)
+                    y1_y = y0 + (h1 * 0.5) - (y1_h * 0.4)
+                    textBox(y1_caption, (y1_x, y1_y, y1_w, y1_h), align='center')
+
+                # draw box for outcoming BCP
                 if self.draw_out and not int(w2) == 0 and not int(h2) == 0:
+
+                    # draw box
                     fill(None)
                     c = self.color + (self.stroke_alpha,)
                     stroke(*c)
                     rect(x0, y0, w2, h2)
                     line((x0, y0), (x2, y2))
 
-                    if y2 > y0:
-                        y2_ = (y2 + 4) * scale
-                    else:
-                        y2_ = (y2 - 14) * scale
-
-                    if x2 > x0:
-                        x2_ = (x2 + 4) * scale
-                    else:
-                        x2_ = (x2 - 19) * scale
-
+                    # draw captions
                     stroke(None)
                     fill(*self.color)
-                    text('%.2f' % abs(x2), (x0 + w2 * 0.5, y2_))
-                    text('%.2f' % abs(y2), (x2_, y0 + h2 * 0.5))
+
+                    # draw x caption
+                    x2_caption = '%.2f' % abs(w2)
+                    x2_w, x2_h = textSize(x2_caption)
+                    x2_x = x0 + (w2 * 0.5) - (x2_w * 0.5)
+                    x2_y = y2 - x2_h * 0.4
+                    textBox(x2_caption, (x2_x, x2_y, x2_w, x2_h), align='center')
+
+                    # draw y caption
+                    y2_caption = '%.2f' % abs(h2)
+                    y2_w, y2_h = textSize(y2_caption)
+                    y2_x = x2 - (y2_w * 0.5)
+                    y2_y = y0 + (h2 * 0.5) - (y2_h * 0.4)
+                    textBox(y2_caption, (y2_x, y2_y, y2_w, y2_h), align='center')
 
         restore()
 
     def _draw_angles(self, scale):
 
-        d = 0.25
         f = BEZIER_ARC_CIRCLE
 
         save()
@@ -187,113 +211,139 @@ class HandlesMeasures(object):
                 x0, y0 = pt.anchor
                 w1, h1 = pt.bcpIn
                 w2, h2 = pt.bcpOut
-                x1, y1 = x0 + w1, y0 + h1
-                x2, y2 = x0 + w2, y0 + h2
 
+                # draw angles for incoming BCP
                 if self.draw_in and not int(w1) == 0 and not int(h1) == 0:
 
-                    distance, angle = get_vector((x0, y0), (x1, y1))
-                    r = distance * d * self.radius
+                    x1 = x0 + w1
+                    y1 = y0 + h1
+
+                    handle_length, angle = get_vector((x0, y0), (x1, y1))
+                    r = handle_length * self.radius
+
+                    a1 = angle % 90
+                    a2 = 90 - a1
 
                     if w1 > 0 and h1 > 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x3, y3 = vector((x0, y0), angle - a1 * 0.5, distance * d)
-                        x4, y4 = vector((x0, y0), angle + a2 * 0.5, distance * d)
+
+                        x3, y3 = vector((x0, y0), angle - a1 * 0.5, r)
+                        x4, y4 = vector((x0, y0), angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 + r, y0
                         p2_x, p2_y = x0, y0 + r
                         p3_x, p3_y = p1_x, p1_y + r * f
                         p4_x, p4_y = p2_x + r * f, p2_y
 
                     elif w1 > 0 and h1 < 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x3, y3 = vector((x0, y0), angle - a1 * 0.5, distance * d)
-                        x4, y4 = vector((x0, y0), angle + a2 * 0.5, distance * d)
+
+                        x3, y3 = vector((x0, y0), angle - a1 * 0.5, r)
+                        x4, y4 = vector((x0, y0), angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 + r, y0
                         p2_x, p2_y = x0, y0 - r
                         p3_x, p3_y = p1_x, p1_y - r * f
                         p4_x, p4_y = p2_x + r * f, p2_y
 
                     elif w1 < 0 and h1 < 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x3, y3 = vector((x0, y0), 180 + angle - a1 * 0.5, distance * d)
-                        x4, y4 = vector((x0, y0), 180 + angle + a2 * 0.5, distance * d)
+
+                        x3, y3 = vector((x0, y0), 180 + angle - a1 * 0.5, r)
+                        x4, y4 = vector((x0, y0), 180 + angle + a2 * 0.5, r)
+
                         p2_x, p2_y = x0 - r, y0
                         p1_x, p1_y = x0, y0 - r
                         p3_x, p3_y = p1_x - r * f, p1_y
                         p4_x, p4_y = p2_x, p2_y - r * f
 
                     else:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x3, y3 = vector((x0, y0), 180 + angle - a1 * 0.5, distance * d)
-                        x4, y4 = vector((x0, y0), 180 + angle + a2 * 0.5, distance * d)
+
+                        x3, y3 = vector((x0, y0), 180 + angle - a1 * 0.5, r)
+                        x4, y4 = vector((x0, y0), 180 + angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 - r, y0
                         p2_x, p2_y = x0, y0 + r
                         p3_x, p3_y = p1_x, p1_y + r * f
                         p4_x, p4_y = p2_x - r * f, p2_y
 
+                    # draw angle arch
                     c = self.color + (self.stroke_alpha,)
                     stroke(*c)
                     fill(None)
-
                     newPath()
                     moveTo((p1_x, p1_y))
                     curveTo((p3_x, p3_y), (p4_x, p4_y), (p2_x, p2_y))
                     drawPath()
 
+                    # draw angle captions
                     stroke(None)
                     fill(*self.color)
-                    text('%.2f' % a1, (x3, y3))
-                    text('%.2f' % a2, (x4, y4))
 
+                    # caption angle 1
+                    caption_a1 = '%.2f' % a1
+                    a1_w, a1_h = textSize(caption_a1)
+                    a1_x = x3 - (a1_w * 0.5)
+                    a1_y = y3 - (a1_h * 0.4)
+                    textBox(caption_a1, (a1_x, a1_y, a1_w, a1_h), align='center')
+
+                    # caption angle 2
+                    caption_a2 = '%.2f' % a2
+                    a2_w, a2_h = textSize(caption_a2)
+                    a2_x = x4 - (a2_w * 0.5)
+                    a2_y = y4 - (a2_h * 0.5)
+                    textBox(caption_a2, (a2_x, a2_y, a2_w, a2_h), align='center')
+
+                # draw angles for outcoming BCP
                 if self.draw_out and not int(w2) == 0 and not int(h2) == 0:
 
-                    distance, angle = get_vector((x0, y0), (x2, y2))
-                    r = distance * d * self.radius
+                    x2 = x0 + w2
+                    y2 = y0 + h2
+
+                    handle_length, angle = get_vector((x0, y0), (x2, y2))
+                    r = handle_length * self.radius
+
+                    a1 = angle % 90
+                    a2 = 90 - a1
 
                     if w2 > 0 and h2 > 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x5, y5 = vector((x0, y0), angle - a1 * 0.5, distance * d)
-                        x6, y6 = vector((x0, y0), angle + a2 * 0.5, distance * d)
+
+                        x5, y5 = vector((x0, y0), angle - a1 * 0.5, r)
+                        x6, y6 = vector((x0, y0), angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 + r, y0
                         p2_x, p2_y = x0, y0 + r
                         p3_x, p3_y = p1_x, p1_y + r * f
                         p4_x, p4_y = p2_x + r * f, p2_y
 
                     elif w2 > 0 and h2 < 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x5, y5 = vector((x0, y0), angle - a1 * 0.5, distance * d)
-                        x6, y6 = vector((x0, y0), angle + a2 * 0.5, distance * d)
+
+                        x5, y5 = vector((x0, y0), angle - a1 * 0.5, r)
+                        x6, y6 = vector((x0, y0), angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0, y0 - r
                         p2_x, p2_y = x0 + r, y0
                         p3_x, p3_y = p1_x + r * f, p1_y
                         p4_x, p4_y = p2_x, p2_y - r * f
 
                     elif w2 < 0 and h2 < 0:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x5, y5 = vector((x0, y0), 180 + angle - a1 * 0.5, distance * d)
-                        x6, y6 = vector((x0, y0), 180 + angle + a2 * 0.5, distance * d)
+
+                        x5, y5 = vector((x0, y0), 180 + angle - a1 * 0.5, r)
+                        x6, y6 = vector((x0, y0), 180 + angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 - r, y0
                         p2_x, p2_y = x0, y0 - r
                         p3_x, p3_y = p1_x, p1_y - r * f
                         p4_x, p4_y = p2_x - r * f, p2_y
 
                     else:
-                        a1 = angle % 90
-                        a2 = 90 - a1
-                        x5, y5 = vector((x0, y0), 180 + angle - a1 * 0.5, distance * d)
-                        x6, y6 = vector((x0, y0), 180 + angle + a2 * 0.5, distance * d)
+
+                        x5, y5 = vector((x0, y0), 180 + angle - a1 * 0.5, r)
+                        x6, y6 = vector((x0, y0), 180 + angle + a2 * 0.5, r)
+
                         p1_x, p1_y = x0 - r, y0
                         p2_x, p2_y = x0, y0 + r
                         p3_x, p3_y = p1_x, p1_y + r * f
                         p4_x, p4_y = p2_x - r * f, p2_y
 
+                    # draw angle arch
                     c = self.color + (self.stroke_alpha,)
                     stroke(*c)
                     fill(None)
@@ -302,10 +352,23 @@ class HandlesMeasures(object):
                     curveTo((p3_x, p3_y), (p4_x, p4_y), (p2_x, p2_y))
                     drawPath()
 
+                    # draw angle captions
                     stroke(None)
                     fill(*self.color)
-                    text('%.2f' % a1, (x5, y5))
-                    text('%.2f' % a2, (x6, y6))
+
+                    # caption angle 1
+                    caption_a1 = '%.2f' % a1
+                    a1_w, a1_h = textSize(caption_a1)
+                    a1_x = x5 - (a1_w * 0.5)
+                    a1_y = y5 - (a1_h * 0.4)
+                    textBox(caption_a1, (a1_x, a1_y, a1_w, a1_h), align='center')
+
+                    # caption angle 2
+                    caption_a2 = '%.2f' % a2
+                    a2_w, a2_h = textSize(caption_a2)
+                    a2_x = x6 - (a2_w * 0.5)
+                    a2_y = y6 - (a2_h * 0.5)
+                    textBox(caption_a2, (a2_x, a2_y, a2_w, a2_h), align='center')
 
         restore()
 
--- make-icon.py
+++ make-icon.py
@@ -1,10 +1,10 @@
 size(20, 20)
 
-m = 0.19
+m = 0.18
 r = 0.12 * width()
 
-sw = 0.6 * r
-sw2 = 0.28 * r
+sw = 0.7 * r
+sw2 = 0.35 * r
 
 p0_x, p0_y = width() * m, height() * m
 p1_x, p1_y = width() * m, height() * (1.0 - m)
@@ -17,7 +17,7 @@ h = height() * (1.0 - m * 2)
 stroke(0)
 lineCap('butt')
 fill(None)
-strokeWidth(sw)
+strokeWidth(sw * 0.75)
 lineDash(None)
 line((p0_x, p0_y), (p3_x, p3_y))
 
@@ -28,16 +28,18 @@ strokeWidth(sw2)
 stroke(0)
 strokeWidth(sw)
 
-r2 = r * 0.5
+r2 = r * 0.7
 
 fill(None)
 strokeWidth(sw2)
+lineDash(sw2*1.5, sw2)
 rect(p0_x, p0_y, w, h)
 
 fill(1)
 strokeWidth(sw)
 lineDash(None)
 rect(p0_x - r, p0_y - r, r * 2, r * 2)
+fill(0)
 rect(p3_x - r2, p3_y - r2, r2 * 2, r2 * 2)
 
 saveImage(['measure-handles-tool-icon.pdf'])
