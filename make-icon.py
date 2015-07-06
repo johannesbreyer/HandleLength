1fb4fd1d1e8332947ee86da903bdef3008ada9fa
Gustavo Ferreira <gustavo@hipertipo.com>
2015-07-06 19:54:19 -0300

    updating icon maker & icon

--- make-icon.py
+++ make-icon.py
@@ -1,60 +1,75 @@
 # make extension icon
 
+# settings
+
 size(20, 20)
 
-# margin
-m = 0.18
+margin = 0.18
+oncurve_point_size = 4.8
+offcurve_point_size = 3.36
+handle_stroke = 1.17
+points_stroke = 1.68
+box_stroke = 0.94
 
-# radius
-r = 0.12 * width()
+save_img = True
 
-# stroke width : handle
-sw = 0.7 * r
+# box points
 
-# stroke width : box
-sw2 = 0.35 * r
+p0_x, p0_y = width() * margin, height() * margin
+p1_x, p1_y = width() * margin, height() * (1.0 - margin)
+p2_x, p2_y = width() * (1.0 - margin), height() * margin
+p3_x, p3_y = width() * (1.0 - margin), height() * (1.0 - margin)
 
-# calculate box points
-p0_x, p0_y = width() * m, height() * m
-p1_x, p1_y = width() * m, height() * (1.0 - m)
-p2_x, p2_y = width() * (1.0 - m), height() * m
-p3_x, p3_y = width() * (1.0 - m), height() * (1.0 - m)
+# box dimensions
 
-# calculate box
-w = width() * (1.0 - m * 2)
-h = height() * (1.0 - m * 2)
+w = width() * (1.0 - margin * 2)
+h = height() * (1.0 - margin * 2)
 
 # draw handle
+
 stroke(0)
-lineCap('butt')
-fill(None)
-strokeWidth(sw * 0.7)
+strokeWidth(handle_stroke)
 lineDash(None)
 line((p0_x, p0_y), (p3_x, p3_y))
 
 # draw box
 
-stroke(0)
-strokeWidth(sw)
-fill(None)
-strokeWidth(sw2)
-lineDash(sw2 * 1.15, sw2 *1.45)
-rect(p0_x, p0_y, w, h)
+box_stroke_radius = box_stroke * 0.5
+box_stroke_steps = 9
+
+stroke(None)
+fill(0)
+
+for j in range(box_stroke_steps):
+    rs_y = p0_y + (j * h / (box_stroke_steps-1))
+
+    for i in range(box_stroke_steps):
+        rs_x = p0_x + (i * w / (box_stroke_steps-1))
+
+        if j == 0 or j == box_stroke_steps - 1:
+            rect(rs_x - box_stroke_radius, rs_y - box_stroke_radius, box_stroke, box_stroke)
+
+        else:
+            if i == 0 or i == box_stroke_steps - 1:
+                rect(rs_x - box_stroke_radius, rs_y - box_stroke_radius, box_stroke, box_stroke)                
 
 # draw on-curve point
 
 fill(1)
-strokeWidth(sw * 1)
+stroke(0)
+strokeWidth(points_stroke)
 lineDash(None)
-rect(p0_x - r, p0_y - r, r * 2, r * 2)
+oncurve_point_radius = oncurve_point_size * 0.5
+
+rect(p0_x - oncurve_point_radius, p0_y - oncurve_point_radius, oncurve_point_size, oncurve_point_size)
 
 # draw control point
 
-r2 = r * 0.7
-
 fill(0)
-oval(p3_x - r2, p3_y - r2, r2 * 2, r2 * 2)
+offcurve_point_radius = offcurve_point_size * 0.5
+oval(p3_x - offcurve_point_radius, p3_y - offcurve_point_radius, offcurve_point_size, offcurve_point_size)
 
 # save image
 
-saveImage(['measure-handles-tool-icon.pdf'])
+if save_img:
+    saveImage(['measure-handles-tool-icon.pdf'])
